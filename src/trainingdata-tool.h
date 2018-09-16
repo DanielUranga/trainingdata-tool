12413932c1a143e3397fc911bcb7f3b27c9ddc43
Daniel Uranga <daniel.uranga@gmail.com>
2018-09-16 17:25:27 -0300

    Add working PGN parsing code

--- src/trainingdata-tool.h
+++
@@ -1,9 +0,0 @@
-﻿// trainingdata-tool.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-#include <position.h>
-
-// TODO: Reference additional headers your program requires here.
