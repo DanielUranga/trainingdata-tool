9bdf87686ffd11a11c7e84f19b6a4e0ca4c32ea1
Daniel Uranga <daniel.uranga@gmail.com>
2018-09-16 11:18:08 -0300

    Scaffolding

---
+++ src/trainingdata-tool.cpp
@@ -0,0 +1,12 @@
+﻿// trainingdata-tool.cpp : Defines the entry point for the application.
+//
+
+#include "trainingdata-tool.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello CMake." << endl;
+    return 0;
+}
---
+++ src/trainingdata-tool.h
@@ -0,0 +1,9 @@
+﻿// trainingdata-tool.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <position.h>
+
+// TODO: Reference additional headers your program requires here.
