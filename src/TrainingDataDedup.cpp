890c85bf7d6d50abc21450cdf91849edb08d5b1b
Daniel Uranga <daniel.uranga@gmail.com>
2019-09-08 12:17:28 -0300

    Correct q_ratio logic

--- src/TrainingDataDedup.cpp
+++ src/TrainingDataDedup.cpp
@@ -10,18 +10,14 @@ float merge_val(float old_val, size_t old_count, float new_val) {
 }
 
 void merge_chunks(lczero::V4TrainingData& chunk, size_t old_count,
-                  const lczero::V4TrainingData& new_chunk,
-                  const float q_ratio) {
+                  const lczero::V4TrainingData& new_chunk) {
   for (size_t i = 0; i < ARR_LENGTH(chunk.probabilities); ++i) {
     chunk.probabilities[i] = merge_val(chunk.probabilities[i], old_count,
                                        new_chunk.probabilities[i]);
   }
 
-  auto Z = static_cast<float>(new_chunk.result);
-  float new_chunk_best_q = new_chunk.best_q * q_ratio + Z * (1.0f - q_ratio);
-  float new_chunk_root_q = new_chunk.root_q * q_ratio + Z * (1.0f - q_ratio);
-  chunk.best_q = merge_val(chunk.best_q, old_count, new_chunk_best_q);
-  chunk.root_q = merge_val(chunk.root_q, old_count, new_chunk_root_q);
+  chunk.best_q = merge_val(chunk.best_q, old_count, new_chunk.best_q);
+  chunk.root_q = merge_val(chunk.root_q, old_count, new_chunk.root_q);
   chunk.best_d = merge_val(chunk.best_d, old_count, new_chunk.best_d);
   chunk.root_d = merge_val(chunk.root_d, old_count, new_chunk.root_d);
 }
@@ -52,6 +48,12 @@ void training_data_dedup(TrainingDataReader& reader, TrainingDataWriter& writer,
 
   while (auto new_chunk = reader.ReadChunk()) {
     total_count++;
+
+    // Average Z and Q depending on q_ratio
+    auto Z = static_cast<float>(new_chunk->result);
+    new_chunk->best_q = new_chunk->best_q * q_ratio + Z * (1.0f - q_ratio);
+    new_chunk->root_q = new_chunk->root_q * q_ratio + Z * (1.0f - q_ratio);
+
     auto elem = chunk_map.find(*new_chunk);
     if (elem == chunk_map.end()) {
       chunk_map.emplace(*new_chunk, 1);
@@ -59,7 +61,7 @@ void training_data_dedup(TrainingDataReader& reader, TrainingDataWriter& writer,
     } else {
       lczero::V4TrainingData merged = elem->first;
       size_t old_count = elem->second;
-      merge_chunks(merged, elem->second, *new_chunk, q_ratio);
+      merge_chunks(merged, elem->second, *new_chunk);
       chunk_map.erase(elem);
       chunk_map.emplace(merged, old_count + 1);
     }
